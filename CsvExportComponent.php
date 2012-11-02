5c70c30353d25ef1e4aa7d1920977a8ffd570446
Ben Graham <bgraham@nextstudio.com.au>
2012-11-02 15:29:39 +1100

    Added support for associated models

--- CsvExportComponent.php
+++ CsvExportComponent.php
@@ -48,7 +48,7 @@ class CsvExportComponent extends Component {
 	private $controller = null;
 
 /**
- * Model we are exporting from, set during initialisation
+ * Maximum number of records to retrieve each time
  * 
  * @var array
  */
@@ -120,24 +120,28 @@ class CsvExportComponent extends Component {
 		$order = $this->getOrder();
 		$filename = $this->formatFilename($filename);
 		
+		$fields = $this->parseFields('slugged');
 		if (!$this->debug) {
 			Configure::write('debug', 0);
 			header('Content-type: text/csv');
 			header('Content-Disposition: attachment; filename="'.$filename.'"');
 		}
-
+/*var_dump($this->parseFields('slugged')); 
+var_dump($this->parseFields('human'));
+var_dump($this->getContainableModels()); exit();*/
 		$params = array(
 			'conditions' => $this->conditions,
-			'fields' => $this->getFields('slugged'),
+			'fields' => $fields,
 			'limit' => $this->limit,
 			'page' => 1,
 			'order' => $order,
+			'contain' => $this->getContainableModels(),
 		);
 
 		$this->openDiskBuffer();
 		$this->writeHeader();
 		while($records = $this->getRecords($params)) {
-			$this->writeRecords($records);
+			$this->writeRecords($records, $fields);
 		}
 		$this->flushDiskBuffer();
 		exit();
@@ -216,7 +220,7 @@ class CsvExportComponent extends Component {
  * @return void
  */
 	private function writeHeader() {
-		$headers = $this->getFields();
+		$headers = $this->parseFields();
 		fputcsv($this->diskBuffer, $headers);
 	}
 
@@ -225,12 +229,21 @@ class CsvExportComponent extends Component {
  * 
  * @access private
  * @param array $records database records
+ * @param array $fields which field names to write, and in which order
  * @return void
  */
-	private function writeRecords($records) {
-		$alias = $this->model->alias;
+	private function writeRecords($records, $fields) {
+		$paths = array();
+		foreach ($fields as $field) {
+			list($model, $column) = explode('.', $field);
+			$paths[] = array('model'=>$model, 'column'=>$column);
+		}
 		foreach ($records as $record) {
-			fputcsv($this->diskBuffer, $record[$alias]);
+			$vals = array();
+			foreach($paths as $path) {
+				$vals[] = $record[ $path['model'] ][ $path['column'] ];
+			}
+			fputcsv($this->diskBuffer, $vals);
 		}
 	}
 
@@ -288,29 +301,171 @@ class CsvExportComponent extends Component {
  * Return normalised fields in associative machine=>human format,
  * or just an indexed array of only machine or only human values.
  * 
- * @param string $format One of 'both', 'slugged', 'human'
+ * @param string $output One of 'both', 'slugged', 'human', or 'model'
  * @return mixed Fields, or false if param is dumb
  */
-	public function getFields($format='both') {
+	private function parseFields($output='both') {
 		$fields = ($this->fields===null) ? array_keys($this->model->schema()) : $this->fields;
 		$normalisedFields = array();
+		$models = array();
 
 		foreach ($fields as $key => $value) {
-			if (is_numeric($key)) {
-				$normalisedFields[$value] = Inflector::humanize($value);
-			}
-			else {
-				$normalisedFields[$key] = $value;
-			}
+			$hasHuman = !is_numeric($key);
+			$field = $hasHuman ? $key : $value;
+			$title = $hasHuman ? $value : null;
+			$Field = new Field($field, $title);
+			$Field->setDefaultModel($this->model->alias);
+			$normalisedFields[$Field->getField()] = $Field->getTitle();
+			$models[] = $Field->getModel();
 		}
-		if ($format==='both') {
+		if ($output==='both') {
 			return $normalisedFields;
-		} elseif ($format==='slugged') {
+		} elseif ($output==='slugged') {
 			return array_keys($normalisedFields);
-		} elseif ($format==='human') {
+		} elseif ($output==='human') {
 			return array_values($normalisedFields);
+		} elseif ($output==='model') {
+			return $models;
 		} else {
 			return false;
 		}
 	}
+
+/**
+ * Return an array of models which should be 'contained' during find,
+ * based on the fields requested for the export.
+ * 
+ */
+	private function getContainableModels() {
+		$models = $this->parseFields('model');
+		$models = array_unique($models);
+		$key = array_search($this->model->alias, $models);
+		if ($key !== false) {
+			unset($models[$key]);
+		}
+		return $models;
+	}
+
+}
+
+
+/**
+ * Handles normalisation of field specifications and humanisation
+ * of field names where necessary.
+ * 
+ * @author bgraham
+ */
+class Field extends Object {
+
+/**
+ * Field specification as provided to constructor
+ * 
+ * @var String
+ */
+	private $field = null;
+
+/**
+ * Human-readable title of field, potentially derived from
+ * the machine-readable form of the field.
+ * 
+ * @var String
+ */
+	private $title = null;
+
+/**
+ * The model to use in the field specification if none is
+ * present.
+ * 
+ * @var mixed
+ */
+	private $defaultModel = null;
+
+/**
+ * Constructor. 
+ * 
+ * @param String $field CakePHP field specification
+ * @param String $title optional human-readable title for this field.
+ * @return instance of self
+ */
+	public function Field($field, $title=null) {
+		$this->field = $field;
+		$this->title = $title;
+		return $this;
+	}
+
+/**
+ * Sets the default model for this Field
+ * 
+ * @param String $model Model name, e.g. "Product"
+ */
+	public function setDefaultModel($model) {
+		$this->defaultModel = $model;
+		return $this;
+	}
+
+/**
+ * Return a field formatted like "Model.column"
+ * 
+ * @return String 
+ */
+	public function getField() {
+		return $this->normalise($this->field);
+	}
+
+/**
+ * Return a human-readable field title like "Column"
+ * 
+ * @return String 
+ */
+	public function getTitle() {
+		if ($this->title !== null) {
+			return $this->title;
+		}
+		return Inflector::humanize($this->getColumn());
+	}
+
+/**
+ * Return the model of this field's specification, e.g.
+ * "Model" from "Model.column"
+ * 
+ * @return String 
+ */
+	public function getModel() {
+		list($model, $column) = explode('.', $this->getField());
+		return $model;
+	}
+
+/**
+ * Return the model of this field's specification, e.g.
+ * "column" from "Model.column"
+ * 
+ * @return String 
+ */
+	public function getColumn() {
+		list($model, $column) = explode('.', $this->getField());
+		return $column;
+	}
+
+/**
+ * Take field specifications in either 'Model.column' or plain
+ * 'column' format, and normalise them to 'Model.column' format.
+ * 
+ * @param String $field un-normalised field specification
+ * @return void
+ */
+	private function normalise($field) {
+		$hasModel = (strpos($field, '.') !== false);
+
+		if ($hasModel === false && $this->defaultModel === null) {
+			throw new Exception("Field has no model and no default provided");
+		}
+		elseif ($hasModel === false) {
+			$model = $this->defaultModel;
+		}
+		elseif ($hasModel === true) {
+			list($model, $field) = explode('.', $field);
+		}
+		return "{$model}.{$field}";
+	}
+
 }
--- README.markdown
+++ README.markdown
@@ -6,31 +6,74 @@ Usage
 
 Include CsvExport as a Component in your controller.
 
-`var $components = array('CsvExport');`
+```php
+var $components = array('CsvExport');
+```
 
-Use `$this->CsvExport->setConditions(array(...))` if only specific 
-records should be exported.
+To filter which records are exported, you can provide conditions. These 
+look just like the standard conditions array used with `Model->find()`.
 
-Use `$this->CsvExport->setFields(array(...))` to limit which fields
-are exported and to provide friendly names for fields. If an array
-item is numerically indexed then it is assumed this is a DB field
-and a human-friendly name is generated from it.
+```php
+$this->CsvExport->setConditions(array(
+	'Model.group_id' => 12
+));
+```
 
-Use `$this->CsvExport->export($this->Model);` to
-actually do the export thing. This results in CSV output being
-written to the output buffer ("the browser") and execution
-terminating.
+To limit which fields are exported and to provide friendly names for 
+fields. If an array item is numerically indexed then it is assumed 
+this is a DB field and a human-friendly name is generated from it.
+
+```php
+$this->CsvExport->setFields(array(
+	'Model.name_first' => 'First Name', // column will be titled 'First Name'
+	'Model.birthdate',                  // column will be titled 'Birthdate'
+));
+```
+
+To cause the CSV output to appear as a file download in-browser, run
+
+```php
+$this->CsvExport->export($this->Model);
+```
+
+The PHP script is `exit()`ed here.
 
 Debugging
 ----------
 
-`$this->CsvExport->debug = true` will prevent "download file" headers
-being sent, keeping output in the browser. If not set, CakePHP's
-debugging is disabled, as it does not, surprisingly, generate output
-in CSV format.
+```php
+$this->CsvExport->debug = true;
+```
+
+This will prevent "download file" headers being sent, keeping output
+in the browser. This is useful to get a quick idea of what your CSV
+output looks like, without having to have the file open in a CSV
+program. If not set, CakePHP's debugging is disabled, as it
+does not, surprisingly, generate output in CSV format. As a result, 
+errors during export may be masked.
 
 Hooks
 ----------
 
-See setRecordProcessorCallback if records require processing before
-being exported.
+```php
+$this->CsvExport->setRecordProcessorCallback(array($this, 'capitalise'));
+```
+
+In this example, the function `MyController::capitalise()` will be passed every
+record before it is exported. The callback function receives one
+argument which looks like the results from a `Model->find('first')` call:
+
+```php
+array(
+	'Model' => array(
+		'id' => 1,
+		'name' => 'Beans',
+	),
+	'AssociatedModel' => array(
+		'id' => 12,
+		'title' => 'Hats',
+	),
+);
+```
+
+`setRecordProcessorCallback()` accepts any argument that `array_map()` will recognise.
